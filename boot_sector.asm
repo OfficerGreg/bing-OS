62b8a0200924760d940d85752f4df1b06611cc5a
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-05-22 22:12:50 +0200

    f

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,16 +1,34 @@
-mov ah, 0x0e        ;teletype BIOS routine
+[org 0x7c00] ; tell the assembler that our offset is bootsector code
 
-mov bp, 0x7ff0
-
-%include "print.asm"
-
-mov al, "X"
+; The main routine makes sure the parameters are ready and then calls the function
+mov bx, HELLO
 call print
 
-mov al, "T"
+call print_nl
+
+mov bx, GOODBYE
 call print
 
+call print_nl
 
+mov dx, 0x12fe
+call print_hex
+
+; that's it! we can hang now
 jmp $
-times 510 - ($-$$) db 0
+
+; remember to include subroutines below the hang
+%include "print.asm"
+%include "print_hex.asm"
+
+
+; data
+HELLO:
+    db 'Hello, World', 0
+
+GOODBYE:
+    db 'Goodbye', 0
+
+; padding and magic number
+times 510-($-$$) db 0
 dw 0xaa55
--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-nasm boot_sector.asm -f bin -o boot_sector.bin
 
-qemu-system-i386 -fda boot_sector.bin
+nasm -f bin -o boot_sector.bin boot_sector.asm
+nasm -f bin -o print.bin print.asm
+nasm -f bin -o print_hez.bin print_hex.asm
+
+cat boot_sector.bin print.bin print_hex.bin > main.bin
+
+qemu-system-i386 -fda main.bin
