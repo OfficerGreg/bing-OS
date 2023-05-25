54952d3bc066a99f1e73afbee4966ba42c594af0
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-05-25 16:08:19 +0200

    32bit

---
+++ src/32bit_print.asm
@@ -0,0 +1,27 @@
+[bits 32]
+
+;constant definitions
+VIDEO_MEMORY equ 0xb8000    ;
+WHITE_ON_BLACK equ 0x0f     ;color byte
+
+private_string_pm:
+    pusha
+    mov edx, VIDEO_MEMORY
+
+private_string_pm_loop:
+    mov al, [ebx]
+    mov ah, WHITE_ON_BLACK
+
+    cmp al, 0
+    je print_string_pm_done
+
+    mov [edx], ax
+    add ebx, 1      ;next char
+    add edx, 2      ;next video mem pos
+
+    jmp private_string_pm_loop
+
+
+print_string_pm_done:
+    popa
+    ret
