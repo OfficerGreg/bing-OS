e0aec289c7b402c11cd3772af997094dfa930bbd
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-05-26 19:42:14 +0200

    cleanup

--- Makefile
+++
@@ -1,9 +0,0 @@
-OS: 
-	nasm -f bin -o ./bin/boot_sect_main.bin boot_sect_main.asm
-	cd ./bin/
-	qemu-system-i386 -fda boot_sect_main.bin
-
-
-
-clean:
-	rm *.bin
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ./src/
-nasm -f bin -o ../bin/32bit_main.bin 32bit_main.asm
+cd ./src/boot/
+nasm -f bin -o ../../bin/32bit_main.bin 32bit_main.asm
 
-qemu-system-x86_64 -fda ../bin/32bit_main.bin
+qemu-system-x86_64 -fda ../../bin/32bit_main.bin
